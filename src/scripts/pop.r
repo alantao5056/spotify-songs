7b2dfc3dd3b863e182b2424c55d2e8be95f740df
alantao5056 <alantaolr@outlook.com>
2024-06-26 15:03:32 -0400

    finished plots and published

--- src/scripts/pop.r
+++ src/scripts/pop.r
@@ -16,4 +16,78 @@ spotify_30000 |>
        color = "Subgenre",
        caption = "30000 Spotify Songs")
 
-ggsave("src/plots/spotify_30000_pop_popularity.png", width = 8, height = 6)
+ggsave("src/plots/pop/spotify_30000_pop_popularity.png", width = 8, height = 6)
+
+spotify_30000 |>
+  filter(playlist_genre == "pop") |>
+  ggplot(aes(x = danceability)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       danceability,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Danceability of Pop Songs By Subgenre",
+       subtitle = "Dance pop is not significanlty more danceable than other genres of pop",
+       x = "Danceability",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/pop/spotify_30000_pop_danceability.png", width = 8, height = 6)
+
+
+spotify_30000 |>
+  filter(playlist_genre == "pop") |>
+  ggplot(aes(x = speechiness)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       speechiness,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Speechiness of Pop Songs By Subgenre",
+       subtitle = "Not speechy",
+       x = "Speechiness",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/pop/spotify_30000_pop_speechiness.png", width = 8, height = 6)
+
+spotify_30000 |>
+  filter(playlist_genre == "pop") |>
+  ggplot(aes(x = valence)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       valence,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Valence of Pop Songs By Subgenre",
+       subtitle = "Evenly spread",
+       x = "Valence",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/pop/spotify_30000_pop_valence.png", width = 8, height = 6)
+
+spotify_30000 |>
+  filter(playlist_genre == "pop") |>
+  ggplot(aes(x = tempo)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       tempo,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Tempo of Pop Songs By Subgenre",
+       subtitle = "Peaks at 100 and 125 BPM",
+       x = "Tempo (BPM)",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/pop/spotify_30000_pop_tempo.png", width = 8, height = 6)
+
--- src/scripts/rock.R
+++ src/scripts/rock.R
@@ -2,6 +2,98 @@ library(tidyverse)
 
 spotify_30000 |>
   filter(playlist_genre == "rock") |>
+  ggplot(aes(x = track_popularity)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       track_popularity,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Popularity of Rock Songs By Subgenre",
+       subtitle = "Permanent Wave is the most popular",
+       x = "popularity",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/rock/spotify_30000_rock_popularity.png", width = 8, height = 6)
+
+spotify_30000 |>
+  filter(playlist_genre == "rock") |>
+  ggplot(aes(x = danceability)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       danceability,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Danceability of Rock Songs By Subgenre",
+       subtitle = "Hard Rock is not as danceable",
+       x = "Danceability",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/rock/spotify_30000_rock_danceability.png", width = 8, height = 6)
+
+
+spotify_30000 |>
+  filter(playlist_genre == "rock") |>
+  ggplot(aes(x = speechiness)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       speechiness,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Speechiness of Rock Songs By Subgenre",
+       subtitle = "Hard rock is more speechy",
+       x = "Speechiness",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/rock/spotify_30000_rock_speechiness.png", width = 8, height = 6)
+
+spotify_30000 |>
+  filter(playlist_genre == "rock") |>
+  ggplot(aes(x = valence)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       valence,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Valence of Rock Songs By Subgenre",
+       subtitle = "Hard Rock has lower valence",
+       x = "Valence",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/rock/spotify_30000_rock_valence.png", width = 8, height = 6)
+
+spotify_30000 |>
+  filter(playlist_genre == "rock") |>
+  ggplot(aes(x = tempo)) +
+  geom_density(aes(color = fct_reorder(playlist_subgenre,
+                                       tempo,
+                                       .desc = TRUE)),
+               alpha = 0.5,
+               size = 1) +
+  theme_bw() +
+  labs(title = "Tempo of Rock Songs By Subgenre",
+       subtitle = "Similar tempos, peaking at 125 BPM",
+       x = "Tempo (BPM)",
+       y = "Density",
+       color = "Subgenre",
+       caption = "30000 Spotify Songs")
+
+ggsave("src/plots/rock/spotify_30000_rock_tempo.png", width = 8, height = 6)
+
+
+spotify_30000 |>
+  filter(playlist_genre == "rock") |>
   ggplot(aes(x = loudness)) +
   geom_density(aes(color = fct_reorder(playlist_subgenre,
                                        loudness,
@@ -16,4 +108,4 @@ spotify_30000 |>
        color = "Subgenre",
        caption = "30000 Spotify Songs")
 
-ggsave("src/plots/spotify_30000_rock_loudness.png", width = 8, height = 6)
+ggsave("src/plots/rock/spotify_30000_rock_loudness.png", width = 8, height = 6)
